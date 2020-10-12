f0bc7dbc0eae07303f77fb9d724d13a4bfccef18
juan <jdgn94@gmail.com>
2020-10-12 09:21:06 -0400

    prueba de una ruta para ver si llegan las peticiones al bot

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-const bot = require('./bot');
+const express = require("express");
+const serveStatic = require("serve-static");
+const bot = require("./bot");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send();
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('listen on port', port);
+console.log("listen on port", port);
 bot.launch();
