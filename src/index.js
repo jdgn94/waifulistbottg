465aa00d905d8b5350b072bb4fc04065f9f86daf
jdgn94 <jdgn94@gmail.com>
2020-10-13 17:43:09 -0400

    deploy para heroku

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV == 'development') require('dotenv').config();
 const express = require("express");
 const bot = require("./bot");
 
-// const TOKEN_TG = process.env.TOKEN_TG;
+const TOKEN_TG = process.env.TOKEN_TG;
 const HEROKU_BASE_URL = process.env.HEROKU_BASE_URL;
 const PORT = process.env.PORT || 3000;
 const PATH_BOT = process.env.PATH_BOT || '/bot_test';
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get(PATH_BOT, (req, res) => {
+app.get('/', (req, res) => {
   res.send("Hello world");
 });
 
@@ -21,8 +21,8 @@ app.get(PATH_BOT, (req, res) => {
 // bot.telegram.setWebhook(`${HEROKU_BASE_URL}/bot${TOKEN_TG}`);
 // bot.startWebhook('/webhook', null, PORT);
 
-app.use(bot.webhookCallback(PATH_BOT));
-bot.telegram.setWebhook(`${HEROKU_BASE_URL}${PATH_BOT}`);
+bot.telegram.setWebhook(`${HEROKU_BASE_URL}/bot${TOKEN_TG}`);
+app.use(bot.webhookCallback(`/bot${TOKEN_TG}`));
 
 app.listen(PORT);
 // launch
