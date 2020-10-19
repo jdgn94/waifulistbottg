df4ea03a9a578a6f3cdf773561efbb6958b2ab0a
jdgn94 <jdgn94@gmail.com>
2020-10-19 17:04:01 -0400

    se arregla un error el el polling

--- src/bot.js
+++ src/bot.js
@@ -5,20 +5,7 @@ const bot = new Telegraf(process.env.TOKEN_TG);  // poner el tocken en una varia
 // const telegram = new Telegram(process.env.TOKEN_TG)
 
 // bot.startPolling();
-if (bot.isPolling()) {
-  reinitPolling();
-} else {
-  initPolling();
-}
-
-async function initPolling() {
-  await bot.startPolling();
-}
-
-async function reinitPolling() {
-  await bot.stopPolling();
-  await initPolling();
-}
+bot.startPolling();
 
 // Init
 bot.start(async ctx => { // hace la llamada para agregar el chat a la bd
